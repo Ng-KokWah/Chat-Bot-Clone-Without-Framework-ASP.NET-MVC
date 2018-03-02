dbc84b35cf98974060f331948d57bd28626053b9
Ng Kok Wah <kokwah98@gmail.com>
2018-03-03 00:53:50 +0800

    Add files via upload

---
+++ ChatBot/Models/BotReply.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatBot.Models
+{
+    public class BotReply
+    {
+        public string usertyped { get; set; }
+        public string usertime { get; set; }
+        public string message { get; set; }
+        public string timestamp { get; set; }
+    }
+}
---
+++ ChatBot/Models/UserMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatBot.Models
+{
+    public class UserMessage
+    {
+        public string msg { get; set; }
+        public string dateandtime { get; set; }
+    }
+}
